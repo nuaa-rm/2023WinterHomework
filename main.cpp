36c483c79d3dc08693a726966b31e46c36bca50d
PatekGao <1315587032@qq.com>
2023-02-05 05:14:04 +0800

    [milestone]应该是没问题了

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <utility>
-#include <unordered_map>
 
 using namespace std;
 using namespace cv;
@@ -36,22 +35,83 @@ paint_line_if_color(const cv::Mat &image, const cv::Point &point1, const cv::Poi
     return lines;
 }
 
-float angle(int x1, int y1, int x2, int y2) {
-    float angle_temp;
-    int xx, yy;
-    xx = x2 - x1;
-    yy = y2 - y1;
-    if (xx == 0.0)
-        angle_temp = M_PI / 2.0;
-    else
-        angle_temp = std::atan(std::fabs(yy / xx));
-    if ((xx < 0.0) && (yy >= 0.0))
-        angle_temp = M_PI - angle_temp;
-    else if ((xx < 0.0) && (yy < 0.0))
-        angle_temp = M_PI + angle_temp;
-    else if ((xx >= 0.0) && (yy < 0.0))
-        angle_temp = M_PI * 2.0 - angle_temp;
-    return (angle_temp);
+cv::Vec4f get_line(const cv::Point &point1, const cv::Point &point2) {
+    float k, b;
+    if (point1.x == point2.x) {
+        k = 0;
+        b = point1.x;
+        return {1, 0, -b, 0};
+    } else {
+        k = (float) (point2.y - point1.y) / (point2.x - point1.x);
+        b = point1.y - k * point1.x;
+        return {k, -1, 0, b};
+    }
+}
+
+cv::Vec4f line_1, line_2;
+
+void get_parallel_lines(cv::Vec4f line, float distance) {
+    cv::Point point1, point2;
+    float k = line[0] / line[1];
+    float b = line[3] / line[1];
+    if (k == 0) {
+        point1 = cv::Point(b - distance, 0);
+        point2 = cv::Point(b + distance, 500);
+    } else if (std::abs(k) > 9999) {
+        point1 = cv::Point(0, b - distance);
+        point2 = cv::Point(500, b + distance);
+    } else {
+        point1 = cv::Point(0, b - distance * k);
+        point2 = cv::Point(500, (b + distance) * k);
+    }
+    line_1 = get_line(point1, point2);
+    line_2 = get_line(point2, point1);
+}
+
+bool is_in_area(cv::Point point, cv::Vec4f line1, cv::Vec4f line2) {
+    float x = point.x, y = point.y;
+    float a1 = line1[1] / line1[0], b1 = line1[3] / line1[0];
+    float a2 = line2[1] / line2[0], b2 = line2[3] / line2[0];
+    if (a1 > 9999 || a1 < -9999) {
+        float value2 = y - a2 * x - b2;
+        if (value2 >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (a2 > 9999 || a2 < -9999) {
+        float value1 = y - a1 * x - b1;
+        if (value1 >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        float value1 = y - a1 * x - b1;
+        float value2 = y - a2 * x - b2;
+        if (value1 * value2 >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+int nearest_multiple(int x) {
+    int n = floor((x - 40) / 100);
+    return 50 + 100 * n;
+}
+
+
+std::vector<std::pair<cv::Point, cv::Point>> lines;
+
+void add_to_vector_if_not_exists(const std::pair<cv::Point, cv::Point> &element) {
+    for (const auto &line: lines) {
+        if (line == element) {
+            return;
+        }
+    }
+    lines.push_back(element);
 }
 
 int matrix_size, num_lines;
@@ -88,7 +148,6 @@ int main() {
     Mat img_COPY = img;
     int loc_max = 0;
     vector<Point> points;
-    std::vector<std::pair<cv::Point, cv::Point>> lines;
     Mat nodesMask, edgesMask;
     cv::inRange(img, nodeColor[0], nodeColor[1], nodesMask);
     cv::inRange(img, edgeColor[0], edgeColor[1], edgesMask);
@@ -138,7 +197,7 @@ int main() {
     Canny(edgesMask, edge_s, 50, 150);
     // 检测直线
     vector<Vec4i> line_s;
-    HoughLinesP(edge_s, line_s, 1, CV_PI / 1440, 20, 30, 10);
+    HoughLinesP(edge_s, line_s, 1, CV_PI / 1440, 50, 30, 15);
     int thickness = 1;
     // 在图像上绘制直线
     for (auto l: line_s) {
@@ -146,52 +205,73 @@ int main() {
     }
     // 显示图像
     imshow("Detected Lines", img_COPY);
-    waitKey(0);
-    vector<float> angles;
-    unordered_map<float, vector<Point>> line_map;
-    for (int x = 0; x < points.size(); ++x) {
-        for (int y = x + 1; y < points.size(); ++y) {
-            float tmp_angle = angle(points[x].x, points[x].y, points[y].x, points[y].y);
-            angles.emplace_back(tmp_angle);
-            line_map[tmp_angle].push_back(points[x]);
-            line_map[tmp_angle].push_back(points[y]);
+    for (auto &l: line_s) {
+        int minLength[6];
+        for (int &i: minLength) {
+            i = 999999;
         }
-    }
-
-    for (auto l: line_s) {
-        float tmp_angle = angle(l[0], l[1], l[2], l[3]);
-        for (auto &i: angles) {
-            if (abs(i - tmp_angle) < 0.001) {
-                for (int tmp = 0; tmp <= line_map[i].size(); tmp = tmp + 2) {
-                    int length;
-                    int m0 = (line_map[i][tmp].x - l[0]) * (line_map[i][tmp].x - l[0]) +
-                             (line_map[i][tmp].y - l[1]) * (line_map[i][tmp].y - l[1]);
-                    int n0 = (line_map[i][tmp + 1].x - l[2]) * (line_map[i][tmp + 1].x - l[2]) +
-                             (line_map[i][tmp + 1].y - l[3]) * (line_map[i][tmp + 1].y - l[3]);
-                    int m1 = (line_map[i][tmp].x - l[2]) * (line_map[i][tmp].x - l[2]) +
-                             (line_map[i][tmp].y - l[3]) * (line_map[i][tmp].y - l[3]);
-                    int n1 = (line_map[i][tmp + 1].x - l[0]) * (line_map[i][tmp + 1].x - l[0]) +
-                             (line_map[i][tmp + 1].y - l[1]) * (line_map[i][tmp + 1].y - l[1]);
-                    if(m0+n0>=m1+n1){
-                        length = m1+n1;
-                    } else {
-                        length = m0+n0;
+        Point matched_point_1_1, matched_point_1_2, matched_point_2_1, matched_point_2_2, matched_point_3_1, matched_point_3_2;
+        if (abs(l[0] - l[2]) <= 10) {
+            l[0] = nearest_multiple(l[0]);
+            for (auto &point: points) {
+                if (point.x == l[0]) {
+                    if (abs(point.y - l[1]) < minLength[0]) {
+                        minLength[0] = abs(point.y - l[1]);
+                        matched_point_1_1 = point;
                     }
-
+                    if (abs(point.y - l[3]) < minLength[1]) {
+                        minLength[1] = abs(point.y - l[3]);
+                        matched_point_1_2 = point;
+                    }
                 }
             }
+            if (matched_point_1_1 != matched_point_1_2)
+                add_to_vector_if_not_exists(make_pair(matched_point_1_1, matched_point_1_2));
+        } else if (abs(l[1] - l[3]) <= 10) {
+            l[1] = nearest_multiple(l[1]);
+            for (auto &point: points) {
+                if (point.y == l[1]) {
+                    if (abs(point.x - l[0]) < minLength[2]) {
+                        minLength[2] = abs(point.x - l[0]);
+                        matched_point_2_1 = point;
+                    }
+                    if (abs(point.x - l[2]) < minLength[3]) {
+                        minLength[3] = abs(point.x - l[2]);
+                        matched_point_2_2 = point;
+                    }
+                }
+            }
+            if (matched_point_2_1 != matched_point_2_2)
+                add_to_vector_if_not_exists(make_pair(matched_point_2_1, matched_point_2_2));
+        } else {
+            get_parallel_lines(l, 20);
+            for (auto &point: points) {
+                if (is_in_area(point, line_1, line_2)) {
+                    if (sqrt((point.x - l[0]) * (point.x - l[0]) + (point.y - l[1]) * (point.y - l[1])) <
+                        minLength[4]) {
+                        minLength[4] = sqrt((point.x - l[0]) * (point.x - l[0]) + (point.y - l[1]) * (point.y - l[1]));
+                        matched_point_3_1 = point;
+                    }
+                    if (sqrt((point.x - l[2]) * (point.x - l[2]) + (point.y - l[3]) * (point.y - l[3])) <
+                        minLength[5]) {
+                        minLength[5] = sqrt((point.x - l[2]) * (point.x - l[2]) + (point.y - l[3]) * (point.y - l[3]));
+                        matched_point_3_2 = point;
+                    }
+                }
+            }
+            if (matched_point_3_1 != matched_point_3_2)
+                add_to_vector_if_not_exists(make_pair(matched_point_3_1, matched_point_3_2));
         }
-
     }
 
 
-    for (int i = 0; i < points.size(); ++i) {
-        for (int j = i + 1; j < points.size(); ++j) {
-            auto painted_lines = paint_line_if_color(img, points[i], points[j], Vec3b(211, 204, 255));
-            lines.insert(lines.end(), painted_lines.begin(), painted_lines.end());
-        }
-    }
 
+//    for (int i = 0; i < points.size(); ++i) {
+//        for (int j = i + 1; j < points.size(); ++j) {
+//            auto painted_lines = paint_line_if_color(img, points[i], points[j], Vec3b(211, 204, 255));
+//            lines.insert(lines.end(), painted_lines.begin(), painted_lines.end());
+//        }
+//    }
 
     num_lines = lines.size();
     cout << num_lines << endl;
